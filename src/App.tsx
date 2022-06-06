00a392c9787526dc0883a71394fac4f49f58eb32
detrojanishith007@gmail.com <detroja@softsmartscorp.com>
2022-06-06 15:52:19 +0530

    add currancy and updown

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import { SmartjkcMask } from './inputs/SmartjkcMask';
+// import { SmartjkcUpDown } from './inputs/SmartjkcUpDown';
+import { SmartjkcNumeric } from './inputs/SmartjkcNumeric';
+
+import {
+  SmartjkcMask,
+  SmartjkcUpDown
+} from 'smartjkc';
+
+import "./App.css";
 
 function App() {
+  const [settings, setSettings] = useState<any>({
+    label: "Date Of Birth",
+    value: "02/26/1992",
+    className: "",
+    inputStyle: { borderColor: "#3EB489" },
+    labelStyle: { color: "#3EB489" },
+    mask: "99/99/9999",
+    maskPlaceholder: "#",
+    alwaysShowMask: true,
+  });
+
+  const [settings1, setSettings1] = useState<any>({
+    label: "Salary",
+    value: '25000',
+    prefix: '$',
+    decimalSeparator: '.',
+    decimalsLimit: 2,
+    decimalScale: 2,
+    step: 1,
+    disableGroupSeparators: true,
+    className: "",
+    inputStyle: { borderColor: "#3EB489" },
+    labelStyle: { color: "#3EB489" }
+  });
+
+  const [settings2, setSettings2] = useState<any>({
+    label: "Percentages",
+    value: "40",
+    className: "",
+    inputStyle: { borderColor: "#3EB489" },
+    labelStyle: { color: "#3EB489" },
+    min: 40,
+    max: 100,
+    step: 0.5,
+    precision: 2
+  });
+
+  const onChange = (event: any) => {
+    setSettings((prevState: any) => {
+      return { ...prevState, value: event.target.value };
+    });
+  };
+
+  const onChange1 = (event: any) => {
+    setSettings1((prevState: any) => {
+      return { ...prevState, value: event };
+    });
+  };
+
+  const onChange2 = (event: any) => {
+    setSettings2((prevState: any) => {
+      return { ...prevState, value: event };
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row m-5">
+        <div className="col-3">
+          <SmartjkcMask name={"dob"} settings={settings} onChangeEvent={onChange}></SmartjkcMask>
+        </div>
+        <div className="col-3">
+          <SmartjkcNumeric name={"salary"} settings={settings1} onChangeEvent={onChange1}></SmartjkcNumeric>
+        </div>
+        <div className="col-3">
+          <SmartjkcUpDown name={"Age"} settings={settings2} onChangeEvent={onChange2}></SmartjkcUpDown>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/inputs/SmartjkcMask.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import InputMask from 'react-input-mask';
+
+export interface IInputMaskProps {
+    name: string;
+    settings: ISettingsProps;
+    onChangeEvent: any;
+    onBlurEvent?: any;
+    onFocusEvent?: any;
+    onKeyUpEvent?: any;
+    onKeyDownEvent?: any;
+};
+
+export interface ISettingsProps {
+    id: string;
+    label: string;
+    value: string;
+    className: string;
+    inputStyle: React.CSSProperties;
+    labelStyle: React.CSSProperties;
+    mask: string;
+    maskChar: string;
+    alwaysShowMask: boolean;
+    disabled: boolean;
+    readOnly: boolean;
+    tabIndex: number;
+    placeholder: string;
+};
+
+export const SmartjkcMask: React.FunctionComponent<IInputMaskProps> = props => {
+
+    const { name, settings, onChangeEvent, onBlurEvent, onFocusEvent, onKeyUpEvent, onKeyDownEvent } = props;
+
+    let labelStyle: React.CSSProperties = {
+        marginBottom: '0px',
+        position: 'absolute',
+        paddingLeft: '6px',
+        fontSize: '11px',
+        fontWeight: '700'
+    };
+
+    let inputStyle: React.CSSProperties = {
+        width: '100%',
+        height: '40px',
+        padding: '10px 0px 0px 5px',
+        fontSize: '15px',
+        border: '1px solid',
+        outline: 'none'
+    };
+
+    const onFocus = (event: any) => {
+        event.target.selectionEnd = event.target.selectionStart = 0;
+        onFocusEvent && onFocusEvent(event);
+    };
+
+    const onKeyUp = (event: any) => {
+        if (event.key === '1' ||
+            event.key === '2' ||
+            event.key === '3' ||
+            event.key === '4' ||
+            event.key === '5' ||
+            event.key === '6' ||
+            event.key === '7' ||
+            event.key === '8' ||
+            event.key === '9' ||
+            event.key === '0') {
+            event.target.setSelectionRange(event.target.selectionStart, event.target.selectionEnd + 1);
+        }
+        onKeyUpEvent && onKeyUpEvent(event);
+    };
+
+    return (
+        <div>
+            <label style={{ ...labelStyle, ...settings.labelStyle }} htmlFor={name}>{settings.label ? settings.label : 'Label'}</label>
+            <InputMask
+                id={settings.id ? settings.id : name}
+                name={name}
+                tabIndex={settings.tabIndex ? settings.tabIndex : undefined}
+                disabled={settings.disabled ? settings.disabled : false}
+                readOnly={settings.readOnly ? settings.readOnly : false}
+                placeholder={settings.placeholder ? settings.placeholder : ''}
+                mask={settings.mask ? settings.mask : '99/99/9999'}
+                maskChar={settings.maskChar ? settings.maskChar : '#'}
+                alwaysShowMask={settings.alwaysShowMask ? settings.alwaysShowMask : true}
+                value={settings?.value}
+                className={settings?.className}
+                style={{ ...inputStyle, ...settings.inputStyle }}
+                onChange={onChangeEvent}
+                onBlur={onBlurEvent}
+                onFocus={onFocus}
+                onKeyUp={onKeyUp}
+                onKeyDown={onKeyDownEvent}
+            ></InputMask>
+        </div>
+    );
+};
