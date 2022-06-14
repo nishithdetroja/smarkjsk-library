8d9dd0d855bf835ccd4262c8ed86b333af11229c
Hitesh Patel <imhiteshkanani@gmail.com>
2022-06-14 17:28:58 +0530

    Working Api Lib

---
+++ src/api/SmartjkcApi.tsx
@@ -0,0 +1,113 @@
+import axios from 'axios';
+
+interface Props {
+    url: string,
+    token: string,
+    body?: {}
+}
+
+export const patchData = async ({ url, body, token }: Props) => {
+    const myheader = {
+        headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
+    }
+    try {
+        const { data } = await axios.patch(
+            url,
+            body,
+            { ...myheader },
+        )
+        return {
+            data: data.data,
+            status: 'success',
+            error: data.error,
+            message: data.info,
+        }
+    } catch (error: any) {
+        return {
+            data: null,
+            status: 'error',
+            message: `Error saving`,
+            error: error.response,
+        }
+    }
+}
+
+export const postData = async ({ url, body, token }: Props) => {
+    const myheader = {
+        headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
+    }
+    try {
+        const { data } = await axios.post(
+            url,
+            body,
+            { ...myheader },
+        )
+        return {
+            data: data.data,
+            status: 'success',
+            error: data.error,
+            message: data.info,
+        }
+    } catch (error: any) {
+        return {
+            data: null,
+            status: 'error',
+            message: `Error saving`,
+            error: error.response,
+        }
+    }
+}
+
+export const getData = async ({ url, token }: Props) => {
+    const myheader = {
+        headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
+    }
+    try {
+        const { data } = await axios.get(
+            url,
+            { ...myheader },
+        )
+        return {
+            data: data.data,
+            status: 'success',
+            error: data.error,
+            message: data.info,
+        }
+    } catch (error: any) {
+        return {
+            data: null,
+            status: 'error',
+            message: `Error saving`,
+            error: error.response,
+        }
+    }
+}
+
+export const deleteData = async ({ url, body, token }: Props) => {
+    const myheader = {
+        headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
+    }
+    try {
+        const reqData = {
+            headers: myheader.headers,
+            data: body
+        }
+        const { data } = await axios.delete(
+            url,
+            reqData
+        )
+        return {
+            data: data.data,
+            status: 'success',
+            error: data.error,
+            message: data.info,
+        }
+    } catch (error: any) {
+        return {
+            data: null,
+            status: 'error',
+            message: `Error saving`,
+            error: error.response,
+        }
+    }
+}
